7b693880b97de14c35d38cda59d5db9db521a33b
77lipe <lipevds.13@gmail.com>
2026-02-19 14:24:21 -0300

    style: Jogo de Adivinhar número melhorado!! agora retorna número de tentativas

--- adivinharNum.py
+++ adivinharNum.py
@@ -3,7 +3,8 @@ import random
 numero = random.randint(1,100)
 #print(numero)
 
-tentativa = int(input("Tente adivinhar o número: "))
+tentativa = int(input("Tente adivinhar o número entre 1 - 100: "))
+cont = 1
 
 def acertou(tentativa):
    if tentativa == numero:
@@ -13,11 +14,15 @@ if acertou(tentativa) != True:
     while tentativa != numero:
         if tentativa < numero:
             print(f"\nTente um número maior que {tentativa}")
+            cont += 1
             tentativa = int(input("Nova tentativa: "))
         else:
             print(f"\nTente um número menor que {tentativa}")
+            cont += 1
             tentativa = int(input("Nova tentativa: "))
     if tentativa == numero:
         print(f'\nVocê acertou o número!!\n"{numero}"')
 else:
     print(f'\nVocê acertou o número!!\n"{numero}"')
+
+print(f"Você realizou {cont} tentativas!!")
